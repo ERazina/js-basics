21ffc8a3d193b7a26b0d3cbb949c1f31e21b0a80
Elina <razina_elina@mail.ru>
2017-04-18 17:00:46 +0300

    first commit

---
+++ object_data.js
@@ -0,0 +1,20 @@
+	//object
+		var obj={
+			prop: "yo",//переменная
+			prop: function(){ //метод
+			}
+			},
+		regexp = /baba/g,
+		func = function(){},
+		arr = [1,2,3,"ice-cream"];
+
+		arr[2] = 24;
+
+		obj.prop = "loftblog";
+		// console.log(typeof obj);
+		// console.log(typeof regexp);
+		// console.log(typeof func);
+		// console.log(typeof arr);
+
+		// console.log(obj);
+		// console.log(arr);
