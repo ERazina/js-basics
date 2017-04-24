1613d2c6db8e8268653839cbda44ed392e930195
Elina <razina_elina@mail.ru>
2017-04-24 14:41:38 +0300

    добавила тренировку

--- create_elements.js
+++ create_elements.js
@@ -19,4 +19,27 @@
 	//wrapeedP.parentNode.appendChild(elem);
 	//wrappedP.parentNode.insertBefore(elem, wrappedP);  //вставляем до wrapped элемент elem
 	// wrappedP.parentNode.replaceChild(elem, wrappedP); // заменяем elem на wrapped
+
+
+
+	//тренировка - создание элемента
+	var newP = doc.createElement("p"); //создали тег p
+	var four = doc.getElementById("four"); //нашли элемент с id four
+	newP.innerHTML = "<i>Содержимое тега p</i>"; //впихнули в p содержимое
+	newP.id = "new"; //приписали id тегу p
+	four.parentNode.insertBefore(newP, four); //вставили на страницу перед id four
+
+	four.parentNode.removeChild(newP); //удалили элемент
+
+
+
+	var div = doc.createElement('div');  //создали div
+	div.id = "new_div";                  //написали id
+	div.innerHTML = "<p>Привет Мир!<p>"; //добавили содержимое 
+
+	var last_div = doc.getElementById("last_div"); //нашли элемент
+	last_div.parentNode.insertBefore(div, last_div); //вставили перед last_div
+
+
+
 }()); 
